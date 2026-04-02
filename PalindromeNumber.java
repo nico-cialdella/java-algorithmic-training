f5667bc867693a32490e6240ac11be9e6f41a47a
nico-cialdella <nicolacialdella25@gmail.com>
2026-04-02 11:33:35 +0200

    Add two comments to explain the logic used in the algorithm

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -5,13 +5,17 @@ public class PalindromeNumber {
         int j;
         boolean equal;
 
+        /* Converting the number to a string allows me to use the Two Pointers technique,
+        converging towards the center. */
         String convertedNumber = String.valueOf(number);
         int size = convertedNumber.length();
 
         i = 0;
         j = size - 1;
         equal = true;
-
+        
+        /* The second condition establishes that while the values ​​being compared are equal, 
+        the loop can continue; as soon as two different values ​​are found, the loop stops and the number is recognized as not a palindrome. */
         while (i < j && equal == true) {
             if (convertedNumber.charAt(i) == convertedNumber.charAt(j)) {
                 equal = true;
