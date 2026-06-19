cdb93e81265e549416f90cb287f39b76733e5991
nico-cialdella <nicolacialdella25@gmail.com>
2026-06-19 19:53:27 +0200

    Add comments to EvalRPN.java

--- src/EvalRPN.java
+++ src/EvalRPN.java
@@ -7,6 +7,14 @@ public class EvalRPN {
         int length = tokens.length;
         int temp;
 
+        /* The logic I used to solve this problem is the following: 
+        for each element of the array, I check if it is a number or an operator.
+        If it's a number, I push it onto the stack.
+        Else, I pop the top two numbers from the stack, execute the operation between them, and save the temporary result by pushing it back onto the stack.
+        It's important to push the temporary result at each iteration, so I can make calculations step by step.
+        This way, I arrive at the end with a unique value left on the stack, which represents the final result.
+        */
+
         while (i < length) {
             try {
                 int number = Integer.parseInt(tokens[i]);
@@ -57,9 +65,9 @@ public class EvalRPN {
         String[] arr_2 = {"4","13","5","/","+"};
         String[] arr_3 = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
 
-        int result_arr_1 = test.evalRPN(arr_1);
-        int result_arr_2 = test.evalRPN(arr_2);
-        int result_arr_3 = test.evalRPN(arr_3);
+        int result_arr_1 = test.evalRPN(arr_1); // Expected Output: 9
+        int result_arr_2 = test.evalRPN(arr_2); // Expected Output: 6
+        int result_arr_3 = test.evalRPN(arr_3); // Expected Output: 22
 
         System.out.println("Result for arr_1: " + result_arr_1);
         System.out.println("Result for arr_2: " + result_arr_2);
